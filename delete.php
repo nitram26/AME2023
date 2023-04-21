9464530726c84fff543210f35dc7853e7a682169
nitram26 <124804618+nitram26@users.noreply.github.com>
2023-04-20 19:51:46 -0500

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,14 @@
+<?php
+
+include("conexion.php");
+$con=conectar();
+
+$id=$_GET['id'];
+
+$sql="DELETE FROM alumno  WHERE id='$id'";
+$query=mysqli_query($con,$sql);
+
+    if($query){
+        Header("Location: index.php");
+    }
+?>
---
+++ login.js
@@ -0,0 +1,12 @@
+function login() {
+    var user, pass;
+    user = document.getElementById("usuario").value;
+    pass = document.getElementById("contraseña").value;
+    if (user == "DSI" && pass== "2024") {
+    
+        window.location ="10 RELOJ.HTML"
+
+    }else{
+   alert(`\n \n ERROR AL INGRESAR DATOS  \n POR FAVOR, INTÉNTELO UNA VEZ MÁS`)
+    }
+}  
